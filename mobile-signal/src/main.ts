817e0af5eeb0ac5f89ae2a83ca773d602c6ef3f1
Hasina-Princy21 <hasinarandria18@gmail.com>
2026-01-19 22:58:15 +0300

    config firebase dans main.ts

--- mobile-signal/src/main.ts
+++ mobile-signal/src/main.ts
@@ -22,10 +22,24 @@ import '@ionic/vue/css/display.css';
 
 /**
  * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
  */
+import { VueFire, VueFireAuth } from "vuefire";
+import { initializeApp } from 'firebase/app';
+// Changement ici : import pour la caméra PWA
+import { defineCustomElements } from '@ionic/pwa-elements/loader'; 
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAQCjw0jzH3z36_Px5knH9YWQOVIUOkvIA",
+  authDomain: "firstprojectfirebase-5c25f.firebaseapp.com",
+  projectId: "firstprojectfirebase-5c25f",
+  storageBucket: "firstprojectfirebase-5c25f.firebasestorage.app",
+  messagingSenderId: "958645113026",
+  appId: "1:958645113026:web:99abe637d3243da83a2b5b",
+  measurementId: "G-5DVS5168ZY"
+});
+
+// Ajout ici : Initialisation des éléments de la caméra
+defineCustomElements(window);
 
 /* @import '@ionic/vue/css/palettes/dark.always.css'; */
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
@@ -36,10 +50,14 @@ import './theme/variables.css';
 
 import 'leaflet/dist/leaflet.css';
 
-
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  // Ajout ici : Connexion de VueFire à l'application
+  .use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+  });
 
 router.isReady().then(() => {
   app.mount('#app');
