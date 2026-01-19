ccf13fe992ca2ea710be385cff06ad0d2f0fccc1
Hasina-Princy21 <hasinarandria18@gmail.com>
2026-01-19 22:18:45 +0300

    Correction : Transformation du submodule en dossier reel

---
+++ photo-gallery/Readme.md
@@ -0,0 +1,2 @@
+# lien test postman
+https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQCjw0jzH3z36_Px5knH9YWQOVIUOkvIA
---
+++ photo-gallery/src/views/Tab1Page.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonGrid, IonRow, IonCol, IonImg, IonFab, IonFabButton, IonIcon
+} from '@ionic/vue';
+import { camera } from 'ionicons/icons';
+import { useFirestore, useCollection } from 'vuefire';
+import { collection, addDoc, orderBy, query } from 'firebase/firestore';
+
+const db = useFirestore();
+const photos = useCollection(collection(db, 'photos'));
+
+const takePhoto = async () => {
+  console.log("Ouverture de la camera...");
+  
+}
+
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Gallery photo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+  </ion-page>
+</template>
